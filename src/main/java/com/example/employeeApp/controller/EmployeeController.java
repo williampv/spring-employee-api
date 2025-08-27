ef98f710d545bcd9c221e44eb6c2ff7d72de35cd
William <williampv@gmail.com>
2025-08-27 10:15:42 -0500

    controller created

--- src/main/java/com/example/employeeApp/controller/EmployeeController.java
+++ src/main/java/com/example/employeeApp/controller/EmployeeController.java
@@ -1,4 +1,31 @@
 package com.example.employeeApp.controller;
 
+import com.example.employeeApp.entity.Employee;
+import com.example.employeeApp.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/EmployeeData")
 public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping
+    public List<Employee> getAll() {
+        return employeeService.getEmployees();
+    }
+
+    @PostMapping
+    public void saveUpdate(@RequestBody Employee employee) {
+        employeeService.saveOrUpdate(employee);
+    }
+
+    @DeleteMapping("/{employeeId}")
+    public void delete(@PathVariable("employeeId") Long employeeId) {
+        employeeService.delete(employeeId);
+    }
 }
